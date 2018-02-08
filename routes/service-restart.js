618fb43517035a065722c6659c64769ba1905638
Denís Graña <denisgranha@gmail.com>
2018-02-08 17:33:20 +0100

    support multiple apps

--- routes/service-restart.js
+++ routes/service-restart.js
@@ -4,15 +4,25 @@ const exec = require('child_process').exec;
 
 /* GET home page. */
 router.get('/:podName', function(req, res, next) {
-    const command = `kubectl get deployment ${req.params.podName} -o yaml | kubectl replace force -f -`
-    exec(command, (e, stdo, stde) => {
-        if (e) {
-            res.status(400).json({error: stde})
-        }
-        else {
-            res.status(200).json({restart: stdo})
-        }
-    })
+
+    const processCommandOutput = command => {
+        return new Promise((resolve, reject) => {
+            exec(command, (e, stdo, stde) => {                
+                e ? reject(stde):resolve(stdo);
+            });
+        });
+    }
+
+    const apps = req.params.podName.split(',');
+    Promise.all(
+        apps.map(appName => 
+            processCommandOutput(`kubectl get deployment ${appName} -o yaml | kubectl replace force -f -`)
+        )
+    ).then(
+        result => {res.status(200).json({restart: result})}        
+    ).catch(
+        e => {res.status(400).json({error: e})},
+    )
 });
 
 module.exports = router;
