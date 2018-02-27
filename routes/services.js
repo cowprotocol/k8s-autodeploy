4c9c4ab1025455474165ef2cfbe733bfa326d242
Raul <raul@gnosis.pm>
2018-02-27 21:38:53 +0100

    Add debug messages

--- routes/services.js
+++ routes/services.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const spawnSync = require('child_process').spawnSync
+const debug = require('debug')('k8s-autodeploy:services')
 
 const executeCommand = (command, args, options = undefined) => {
   let result = spawnSync(command, args, options)
@@ -43,15 +44,25 @@ router.post('/:services/restart', function (req, res, next) {
   const dockerTag = req.body.push_data.tag
   const services = req.params.services.split(',')
 
+  debug('NEW restart request. Services => %s, DockerTag => %s', req.params.services, dockerTag)
+
   Promise.all(
     services.map(service => {
       let deploymentName = deployment.getNameFromDockerTag(service, dockerTag)
-      return deployment.restart(deploymentName)
+      if (deploymentName) {
+        return deployment.restart(deploymentName).then(() => {
+          debug('Deployment restarted => %s', deploymentName)
+        })
+      } else {
+        return Promise.reject(new Error(`There is not deployment defined for this docker tag => ${dockerTag}`))
+      }
     })
-  ).then(
-    result => { res.status(200).json({}) }
-  ).catch(error => {
-    res.status(500).json({'error': error})
+  ).then(result => {
+    debug('ALL requested deployments have been restarted => %s', req.params.services)
+    res.status(200).json({})
+  }).catch(error => {
+    debug('[ERROR] Deployment couldn\'t restart. Reason => %s', error)
+    res.status(500).json({})
   })
 })
 
