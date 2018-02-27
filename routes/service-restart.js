ac160b5c25229f671eda16bce8135d42cf8ca73f
Raul <raul@gnosis.pm>
2018-02-27 13:54:05 +0100

    Refactor restart service

--- routes/service-restart.js
+++ routes/service-restart.js
@@ -2,42 +2,50 @@ const express = require('express')
 const router = express.Router()
 const exec = require('child_process').exec
 
+const processCommandOutput = command => {
+  return new Promise((resolve, reject) => {
+    exec(command, (e, stdo, stde) => {
+      e ? reject(stde) : resolve(stdo)
+    })
+  })
+}
+
+const deployment = {
+  saveConfig (deploymentName, outputDir) {
+    return processCommandOutput(`kubectl get deployment ${deploymentName} -o yaml > ${outputDir}/${deploymentName}.yaml`)
+  },
+  delete (deploymentName) {
+    return processCommandOutput(`kubectl delete deployment ${deploymentName}`)
+  },
+  applyConfig (deploymentName, configDir) {
+    return processCommandOutput(`cat ${configDir}/${deploymentName}.yaml | kubectl apply -f -`)
+  }
+}
+
+const getDeploymentName = (serviceName, dockerTag) => {
+  switch (dockerTag) {
+    case 'develop':
+      return `dev-${serviceName}`
+    case 'master':
+      return `staging-${serviceName}`
+  }
+}
+
 /* GET home page. */
 router.post('/:serviceName', function (req, res, next) {
-  const processCommandOutput = command => {
-    return new Promise((resolve, reject) => {
-      exec(command, (e, stdo, stde) => {
-        e ? reject(stde) : resolve(stdo)
-      })
-    })
-  }
-
   // const images = req.body.push_data.images
-  const tag = req.body.push_data.tag
+  const dockerTag = req.body.push_data.tag
   const serviceName = req.params.serviceName
-  let deploymentName
+  const deploymentDir = '/tmp'
+  let deploymentName = getDeploymentName(serviceName, dockerTag)
 
-  switch (tag) {
-    case 'develop':
-      deploymentName = `dev-${serviceName}`
-      break
-    case 'master':
-      deploymentName = `staging-${serviceName}`
-      break
-    default:
-      break
-  }
-
-  processCommandOutput(`kubectl get deployment ${deploymentName} -o yaml > /tmp/${deploymentName}.yaml`)
-  .then(result => {
-    return processCommandOutput(`kubectl delete deployment ${deploymentName}`).then(() => {
-      return processCommandOutput(`cat /tmp/${deploymentName}.yaml | kubectl apply -f -`).then(() => {
-        return res.status(200).json({'restart': 'OK'})
+  deployment.saveConfig(deploymentName, deploymentDir).then(result => {
+    return deployment.delete(deploymentName).then(() => {
+      return deployment.applyConfig(deploymentName, deploymentDir).then(() => {
+        res.status(200).json({})
       })
     })
-  }).catch(
-    error => { res.status(400).json({'error': error}) }
-  )
+  }).catch(error => { res.status(500).json({'error': error}) })
 })
 
 module.exports = router
