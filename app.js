b3f7f230291bd3a99d8fdd1b47cbaa8f6cb415f5
Raul <raul@gnosis.pm>
2018-02-28 10:25:23 +0100

    Delete index page

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var index = require('./routes/index');
 var services = require('./routes/services');
 
 var app = express();
@@ -22,7 +21,6 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
 app.use('/services', services);
 
 // catch 404 and forward to error handler
