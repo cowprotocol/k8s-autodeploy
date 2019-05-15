e5ea68f33d9313d171c32e2f61c73c8893363ce6
Raul <raul@gnosis.pm>
2019-05-15 11:36:51 +0200

    Add `timestamp` to request log

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.set('view engine', 'pug');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
+app.use(logger(':date[iso] :method :url :status :response-time ms - :res[content-length]'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
