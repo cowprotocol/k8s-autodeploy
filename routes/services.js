95222c16dc6997a2d4598fcc2fb64b2fb44b6d26
Giacomo Licari <giacomo.licari@gmail.com>
2020-08-27 16:58:44 +0200

    Add rollout route

--- routes/services.js
+++ routes/services.js
@@ -20,6 +20,22 @@ const executeCommand = (command, args, options = undefined) => {
   }
 }
 
+const commonErrorHandler = (error, response, next) => {
+  if (error instanceof NotSupportedDockerTagError) {
+    debug('[WARNING] Reason => %s', error)
+    response.status(412).json({})
+  } else if (error.includes('NotFound')) {
+    // Replace `Error` word because it is a warning message
+    let warningMessage = error.replace('Error', '')
+
+    debug('[WARNING] %s', warningMessage)
+    response.status(412).json({})
+  } else {
+    // Default error handler
+    next(error)
+  }
+}
+
 const deployment = {
   applyConfig (deploymentName, config) {
     return executeCommand('kubectl', ['apply', '-f', '-'], {input: config})
@@ -46,9 +62,15 @@ const deployment = {
         return this.applyConfig(deploymentName, config)
       })
     })
+  },
+  rollout (deploymentName) {
+    return this.getConfig(deploymentName).then(config => {
+      return executeCommand('kubectl', ['rollout', 'restart', 'deployment/' + deploymentName])
+    })
   }
 }
 
+// Restart deployment
 router.post('/:services/restart', function (request, response, next) {
   const dockerTag = request.body.push_data.tag
   const services = request.params.services.split(',')
@@ -70,19 +92,33 @@ router.post('/:services/restart', function (request, response, next) {
     debug('ALL requested deployments have been restarted => %s', request.params.services)
     response.status(200).json({})
   }).catch(error => {
-    if (error instanceof NotSupportedDockerTagError) {
-      debug('[WARNING] Reason => %s', error)
-      response.status(412).json({})
-    } else if (error.includes('NotFound')) {
-      // Replace `Error` word because it is a warning message
-      let warningMessage = error.replace('Error', '')
+    return commonErrorHandler(error, response, next)
+  })
+})
 
-      debug('[WARNING] %s', warningMessage)
-      response.status(412).json({})
-    } else {
-      // Default error handler
-      next(error)
-    }
+// Rollout deployment
+router.post('/:services/rollout', function (request, response, next) {
+  const dockerTag = request.body.push_data.tag
+  const services = request.params.services.split(',')
+
+  debug('NEW rollout request. Services => %s, DockerTag => %s', request.params.services, dockerTag)
+
+  Promise.all(
+    services.map(service => {
+      let deploymentName = deployment.getNameFromDockerTag(service, dockerTag)
+      if (deploymentName) {
+        return deployment.rollout(deploymentName).then(() => {
+          debug('Deployment rolled out => %s', deploymentName)
+        })
+      } else {
+        return Promise.reject(new NotSupportedDockerTagError(`This image tag is not supported for autodeploying => ${dockerTag}`))
+      }
+    })
+  ).then(result => {
+    debug('ALL requested deployments have been rolled out => %s', request.params.services)
+    response.status(200).json({})
+  }).catch(error => {
+    return commonErrorHandler(error, response, next)
   })
 })
 
